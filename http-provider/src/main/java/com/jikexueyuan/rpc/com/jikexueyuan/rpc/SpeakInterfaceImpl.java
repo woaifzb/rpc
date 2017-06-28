fa874e17dd0559c244484902136468e670ebebf6
[fzb] <fangzhuobin@staff.cntv.cn>
2017-06-28 21:40:01 +0800

    1st commit

---
+++ http-provider/src/main/java/com/jikexueyuan/rpc/com/jikexueyuan/rpc/SpeakInterfaceImpl.java
@@ -0,0 +1,30 @@
+package com.jikexueyuan.rpc.com.jikexueyuan.rpc;
+
+import com.jikexueyuan.rpc.People;
+import com.jikexueyuan.rpc.SpeakInterface;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by version_z on 2015/8/22.
+ */
+@Component("speakInterface")
+public class SpeakInterfaceImpl implements SpeakInterface
+{
+    public String speak(People people)
+    {
+        if (people.getAge() > 18)
+        {
+            if (people.getSex() == 0)
+            {
+                return "男 "+people.getAge()+"岁";
+            }else
+            {
+                return "女  "+people.getAge()+"岁";
+            }
+        }
+        else
+        {
+            return "小朋友";
+        }
+    }
+}
